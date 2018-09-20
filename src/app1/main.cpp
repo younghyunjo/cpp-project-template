2ebeb814ca92f30e20ec858829376054bd1b7a9d
Younghyun Jo <younghyunjo99@gmail.com>
2018-09-20 18:25:35 +0900

    third_party:Delete rapidjson

--- src/app1/main.cpp
+++ src/app1/main.cpp
@@ -1,38 +1,23 @@
 #include <iostream>
 
 #include "cjson/cJSON.h"
-
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
-
-using namespace rapidjson;
-
-int rapidjson_test() {
-    // 1. Parse a JSON string into DOM.
-    const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
-    Document d;
-    d.Parse(json);
-    // 2. Modify it by DOM.
-    Value& s = d["stars"];
-    s.SetInt(s.GetInt() + 1);
-    // 3. Stringify the DOM
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    d.Accept(writer);
-    // Output {"project":"rapidjson","stars":11}
-    std::cout << buffer.GetString() << std::endl;
-    return 0;
-}
+#include "curl/curl.h"
 
 int cjson_test() {
-    cJSON_Version();
+    std::cout << "Curl version:";
+    std::cout << cJSON_Version() <<std::endl;
+}
+
+void curl_test() {
+    std::cout << "Curl version:";
+    std::cout << curl_version() << std::endl;
 }
 
 int main(void)
 {
-	rapidjson_test();
+
 	cjson_test();
+	curl_test();
 
     return 0;
 }
